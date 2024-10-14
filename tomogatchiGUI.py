26ad41c4be6c7fe2ac0a3fabb0b95cb5ef42bce8
Brenner <btsdet@icloud.com>
2024-10-14 16:29:24 -0400

    Update tomogatchiGUI.py
    Fixed Button and Text Field layout

--- tomogatchiGUI.py
+++ tomogatchiGUI.py
@@ -1,23 +1,25 @@
 import tkinter as tk
+import tkinter.ttk
 from tkinter import *
 from tkinter import font
 
 root = tk.Tk()
 
+
 style1 = font.Font(size=25)
 style2 = font.Font(size = 20)
 
-homescreen = Frame(root)
+homescreen = tk.Frame(root, height=350)
 signup = Frame(root)
 savefile = Frame(root)
 mainscreen = Frame(root)
 
-homescreen.grid(row=0, column=0, sticky="nsew")
+homescreen.grid(row=0, column=0, sticky='ew')
 signup.grid(row=0, column=0, sticky="nsew")
 savefile.grid(row=0, column=0, sticky="nsew")
 mainscreen.grid(row=0, column=0, sticky="nsew")
 
-lb0 = Label(homescreen, text="Tomogatchi", font=style1)
+lb0 = Label(homescreen, text="Welcome!", font=style1)
 lb0.pack(pady=20)
 
 lb1 = Label(signup, text="Sign Up", font=style1)
@@ -29,31 +31,41 @@ lb2.pack(pady=30)
 lb3 = Label(mainscreen, text="Pet Name", font=style1)
 lb3.pack(pady=30)
 
-def remove_placeholder(event=None):
+def username_click(event):
+    if username.get() == "Enter Username":
+        username.delete(0, tk.END)
+        username.configure(foreground="black")
+
+def username_focus_out(event):
     if username.get() == "":
-        username_placeholder.place(x=50, y=100)
-    else:
-        username_placeholder.place_forget()
+        username.insert(0, "Enter Username")
+        username.configure(foreground="gray")
+
+def password_click(event):
+    if password.get() == "Enter Password":
+        password.delete(0, tk.END)
+        password.configure(foreground="black")
+
+def password_focus_out(event):
+    if password.get() == "":
+        password.insert(0, "Enter Password")
+        password.configure(foreground="gray")
+
 
 
 username = tk.Entry(homescreen, font=('Bold', 15))
-username.pack(pady=20)
-username.bind('<FocusIn>', remove_placeholder)
-username.bind('<FocusOut>', remove_placeholder)
-
-username_placeholder = tk.Label(homescreen, text='User Name', fg='gray', font=('Bold', 12), bg='white')
-username_placeholder.place(x=50, y=100)
-username_placeholder.bind('<Button-1>', lambda e: username.focus())
+username.insert(0, "Enter Username")
+username.bind('<FocusIn>', username_click)
+username.bind('<FocusOut>', username_focus_out)
+username.pack()
 
 
 password = tk.Entry(homescreen, font=('Bold', 15))
-password.pack(pady=20)
-password.bind('<FocusIn>', remove_placeholder)
-password.bind('<FocusOut>', remove_placeholder)
+password.insert(0, "Enter Password")
+password.bind('<FocusIn>', password_click)
+password.bind('<FocusOut>', password_focus_out)
+password.pack()
 
-password_placeholder = tk.Label(homescreen, text='Password', fg='gray', font=('Bold', 12), bg='white')
-password_placeholder.place(x=50, y=150)
-password_placeholder.bind('<Button-1>', lambda e: password.focus())
 
 signupBut = Button(homescreen, text="Sign Up", command=lambda: signup.tkraise(), font=style2)
 signupBut.pack()
@@ -78,6 +90,11 @@ feedBut.pack()
 
 homescreen.tkraise()
 
+root.geometry("700x500")
+root.title("Tomogatchi")
+root.grid_columnconfigure(0, weight=1)
+root.resizable(False, False)
+root.mainloop()
 root.geometry("700x700")
 root.title("Tomogatchi")
 root.resizable(False, False)
