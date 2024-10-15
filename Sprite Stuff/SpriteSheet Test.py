ece3bb95b5c32d2766ce014df6c7004375a23518
Nat B <nbboulware@gmail.com>
2024-10-14 23:42:02 -0400

    Spritesheet reader added
    complete with a 2x2 array, ready to look at spritesheets!

--- Sprite Stuff/SpriteSheet Test.py
+++ Sprite Stuff/SpriteSheet Test.py
@@ -7,39 +7,52 @@ root = tk.Tk()
 root.geometry('600x400')
 root.title('Images')
 
-image_original = Image.open('Desktop\Repository\Python Repository\Sophmore Project\Squirrel.png')
+image_original = Image.open('Squirrel.png')
 
-#Still a WIP, Turning this into a 2 x 2 array
 class Animal:
     #Image splitter#
     #//NOTE: Will only read in squares!//#
     def FrameSplit(self, img, byteSize): #(Image File, Pixel Count)
-        frame = []
+        def appendList(l, ele):
+            l.append(ele)
+            return l
+
+        framesY = [] 
         #Needed to get the spritesheet size
         x, y = img.size
         frameNumX = int(x/byteSize)
-        frameRowY = int(y/byteSize)
+        frameNumY = int(y/byteSize)
+
+        print(frameNumX, frameNumY)
 
         #Splits the image and adds them to an array
-        i = 0
-        while i < frameNumX:
-            #Frame 1 Example: (x0, y0, x32, y32)
-            j = byteSize * i
-            tempImg = img.crop((0 + j, 0, byteSize + j, byteSize))        
-            frame.append(tempImg)
-            i += 1
-        return frame
-
+        y = 0
+        while y < frameNumY:
+            tempFramesX = []
+            x = 0
+            while x < frameNumX:
+                #Frame 1 Example: (x0, y0, x32, y32)
+                j = byteSize * x
+                k = byteSize * y
+                tempImg = img.crop((0 + j, 0 + k, byteSize + j, byteSize + k))        
+                tempFramesX.append(tempImg)
+                x += 1
+            
+            framesY = appendList(framesY, tempFramesX)
+            y += 1
+        
+        return framesY
     def __init__(self, img, byteSize):
         self.img = img
         self.bitSiz = byteSize
         self.frame = self.FrameSplit(img, byteSize) 
     
-    def FrameGet(self, x):
-        image = self.frame[x]
+    def FrameGet(self, y, x):
+        image = self.frame[y][x]
         return image
 
 squirrel = Animal(image_original, 32)
-squirrel.FrameGet(3).show()
+
+squirrel.FrameGet(0, 0).show()
 
 #root.mainloop()
