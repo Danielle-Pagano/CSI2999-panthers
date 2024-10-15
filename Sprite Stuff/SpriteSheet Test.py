b58ad95eeac70287f235b882790223faf8b17715
Nat B <nbboulware@gmail.com>
2024-10-14 23:47:16 -0400

    Code Cleanup

--- Sprite Stuff/SpriteSheet Test.py
+++ Sprite Stuff/SpriteSheet Test.py
@@ -1,11 +1,5 @@
 import tkinter as tk
-import pygame as pg
-from tkinter import ttk
-from PIL import Image, ImageTk
-
-root = tk.Tk()
-root.geometry('600x400')
-root.title('Images')
+from PIL import Image
 
 image_original = Image.open('Squirrel.png')
 
@@ -13,11 +7,13 @@ class Animal:
     #Image splitter#
     #//NOTE: Will only read in squares!//#
     def FrameSplit(self, img, byteSize): #(Image File, Pixel Count)
+        
+        framesY = [] 
+        
         def appendList(l, ele):
             l.append(ele)
             return l
 
-        framesY = [] 
         #Needed to get the spritesheet size
         x, y = img.size
         frameNumX = int(x/byteSize)
@@ -37,11 +33,12 @@ class Animal:
                 tempImg = img.crop((0 + j, 0 + k, byteSize + j, byteSize + k))        
                 tempFramesX.append(tempImg)
                 x += 1
-            
+
             framesY = appendList(framesY, tempFramesX)
             y += 1
-        
+
         return framesY
+
     def __init__(self, img, byteSize):
         self.img = img
         self.bitSiz = byteSize
@@ -51,8 +48,7 @@ class Animal:
         image = self.frame[y][x]
         return image
 
-squirrel = Animal(image_original, 32)
-
-squirrel.FrameGet(0, 0).show()
-
-#root.mainloop()
+##//Demo//##
+#(Image Object, PixelxPixel count) (X Y)
+#squirrel = Animal(image_original, 32)
+#squirrel.FrameGet(0, 0).show()
