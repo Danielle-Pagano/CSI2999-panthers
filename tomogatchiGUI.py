08de4d985b7fe05128d69e67de8f8d1cebe189a3
Brenner <btsdet@icloud.com>
2024-10-28 01:02:29 -0400

    Add files via upload

--- tomogatchiGUI.py
+++ tomogatchiGUI.py
@@ -1,101 +1,198 @@
 import tkinter as tk
-import tkinter.ttk
+from tkinter import ttk
 from tkinter import *
-from tkinter import font
+import ttkbootstrap as tb
+from PIL import Image, ImageTk
+import tkinter.font as tkFont
 
-root = tk.Tk()
 
+root = tb.Window(themename='morph')
+root.rowconfigure(0)
+root.columnconfigure(0)
 
-style1 = font.Font(size=25)
-style2 = font.Font(size = 20)
+# frame set ups
+homescreen = tk.Frame(root)
+signup = tk.Frame(root)
+savefile = tk.Frame(root)
+mainscreen = tk.Frame(root)
 
-homescreen = tk.Frame(root, height=350)
-signup = Frame(root)
-savefile = Frame(root)
-mainscreen = Frame(root)
-
-homescreen.grid(row=0, column=0, sticky='ew')
+# frame grids
+homescreen.grid(row=0, column=0, sticky='nsew')
 signup.grid(row=0, column=0, sticky="nsew")
 savefile.grid(row=0, column=0, sticky="nsew")
 mainscreen.grid(row=0, column=0, sticky="nsew")
 
-lb0 = Label(homescreen, text="Welcome!", font=style1)
-lb0.pack(pady=20)
+#cannot get this to work right now
 
-lb1 = Label(signup, text="Sign Up", font=style1)
-lb1.pack(pady=20)
+#background=Image.open("images/backgroundPic.jpeg")
+#background=ImageTk.PhotoImage(background)
+#image_label=tk.Label(homescreen,image=background)
+#image_label.pack(pady=20)
 
-lb2 = Label(savefile, text="Select Save File", font=style1)
-lb2.pack(pady=30)
+homescreen.rowconfigure(0)
+homescreen.columnconfigure(0)
 
-lb3 = Label(mainscreen, text="Pet Name", font=style1)
-lb3.pack(pady=30)
+title_label=tb.Label(homescreen, text="Welcome!",anchor=N, font=('Arial',30))
+title_label.pack(padx=10,pady=10)
 
-def username_click(event):
-    if username.get() == "Enter Username":
-        username.delete(0, tk.END)
-        username.configure(foreground="black")
+# creates line beneath title
+home_separator=ttk.Separator(homescreen, orient='horizontal')
+home_separator.pack(fill='x')
 
-def username_focus_out(event):
-    if username.get() == "":
-        username.insert(0, "Enter Username")
-        username.configure(foreground="gray")
+# need to have log in feature/frame
+start_button=tb.Button(homescreen, text="START", bootstyle='success',command=lambda: savefile.tkraise(), cursor='hand2')
+start_button.place(relx=.4,rely=.5, anchor='center')
 
-def password_click(event):
-    if password.get() == "Enter Password":
-        password.delete(0, tk.END)
-        password.configure(foreground="black")
-
-def password_focus_out(event):
-    if password.get() == "":
-        password.insert(0, "Enter Password")
-        password.configure(foreground="gray")
+# goes to sign up frame
+signup_button=tb.Button(homescreen, text="SIGN UP", bootstyle='info', command=lambda: signup.tkraise(), cursor='hand2')
+signup_button.place(relx=.6,rely=.5,anchor='center')
 
 
+signup.rowconfigure(0, weight=1)
+signup.columnconfigure(0, weight=1)
 
-username = tk.Entry(homescreen, font=('Bold', 15))
-username.insert(0, "Enter Username")
-username.bind('<FocusIn>', username_click)
-username.bind('<FocusOut>', username_focus_out)
-username.pack()
+# label frame with user information
+# entry widgets don't do anything yet
+signup_info_frame = tk.LabelFrame(signup, text="User Information")
+signup_info_frame.grid(row=0,column=0, padx=20, pady=20,sticky='nsew')
 
+first_name = tk.Label(signup_info_frame, text="First Name")
+first_name.grid(row=0, column=0)
 
-password = tk.Entry(homescreen, font=('Bold', 15))
-password.insert(0, "Enter Password")
-password.bind('<FocusIn>', password_click)
-password.bind('<FocusOut>', password_focus_out)
-password.pack()
+last_name = tk.Label(signup_info_frame, text="Last Name")
+last_name.grid(row=0, column=1)
 
+first_name_entry = tk.Entry(signup_info_frame)
+last_name_entry = tk.Entry(signup_info_frame)
 
-signupBut = Button(homescreen, text="Sign Up", command=lambda: signup.tkraise(), font=style2)
-signupBut.pack()
+first_name_entry.grid(row=1, column=0)
+last_name_entry.grid(row=1, column=1)
 
-loginBut = Button(homescreen, text="Log In", command=lambda: savefile.tkraise(), font=style2)
-loginBut.pack()
+email = tk.Label(signup_info_frame,text="Email")
+email.grid(row=0, column=2)
 
-CAButton = Button(signup, text="Create Account", command=lambda: savefile.tkraise(), font=style2)
-CAButton.pack()
+email_entry = tk.Entry(signup_info_frame)
+email_entry.grid(row=1, column=2)
 
-newSaveBut = Button(savefile, text="New File", command=lambda: mainscreen.tkraise(), font=style2)
-newSaveBut.pack()
+# can enter manually as well
+age_label = tk.Label(signup_info_frame, text="Age")
+age_spinbox=tk.Spinbox(signup_info_frame, from_=1, to=100)
 
-continueBut = Button(savefile, text="Continue", command=lambda: mainscreen.tkraise(), font=style2)
-continueBut.pack()
+age_label.grid(row=2, column=0)
+age_spinbox.grid(row=3, column=0)
 
-playBut = Button(mainscreen, text="Play", font=style2)
-playBut.pack()
+# can enter manually as well
+birthday_label=tk.Label(signup_info_frame, text="Birthday")
+birthday_label.grid(row=2, column=1)
 
-feedBut = Button(mainscreen, text="Feed", font=style2)
-feedBut.pack()
+birthday_entry=tb.DateEntry(signup_info_frame,bootstyle="light")
+birthday_entry.grid(row=3,column=1)
+
+for widget in signup_info_frame.winfo_children():
+    widget.grid_configure(padx=10,pady=5)
+
+# drop down only contains squirrel at the moment
+animal_selection_frame=tk.LabelFrame(signup, text="Animal")
+animal_selection_frame.grid(row=1,column=0, sticky="news", padx=20, pady=20)
+
+# name will be saved moving forward
+select_animal=tk.Label(animal_selection_frame, text="Select Animal")
+select_animal.grid(row=0, column=0)
+
+animal_chosen=ttk.Combobox(animal_selection_frame, values = ["Squirrel"])
+animal_chosen.grid(row=1, column=0)
+
+animal_name=tk.Label(animal_selection_frame, text="Animal's Name")
+animal_name.grid(row=0,column=2)
+
+animal_name_entry=tk.Entry(animal_selection_frame)
+animal_name_entry.grid(row=1, column=2)
+
+for widget in animal_selection_frame.winfo_children():
+    widget.grid_configure(padx=10,pady=5)
+
+# goes back to home screen
+home_button=tb.Button(signup, text="Home",bootstyle = 'light',command=lambda: homescreen.tkraise(),cursor='hand2')
+home_button.grid(row=2, column=0, sticky='sw',padx=5,pady=5)
+
+# if all required fields are satisfied, will navigate to save files
+create_account_button=tb.Button(signup,text="Create Account",bootstyle = 'success', command=lambda: savefile.tkraise(),cursor='hand2')
+create_account_button.grid(row=2, column=0, sticky='se',padx=5,pady=5)
+
+savefile.rowconfigure(0, weight=1)
+savefile.columnconfigure(0, weight=1)
+
+continue_file_frame=tk.LabelFrame(savefile,text="Save File 1")
+continue_file_frame.grid(row=0, column=0, padx=20,pady=20,sticky='nsew')
+
+# continues save data; goes to play screen with pet
+continue_button=tb.Button(continue_file_frame, text="Continue", bootstyle='success', command=lambda: mainscreen.tkraise(), cursor='hand2')
+continue_button.grid(row=0, column=0,sticky='ew')
+
+# deletes save data and makes it a "new file"
+delete_save_button=tb.Button(continue_file_frame,text="Delete Save", bootstyle='danger', cursor='hand2')
+delete_save_button.grid(row=1,column=0,sticky='ew')
+
+# doesnt display anything because empty save file
+animal_name2=tk.Label(continue_file_frame,text="Animal Name")
+animal_name2.grid(row=0,column=1)
+
+# doesnt display anything because empty save file
+time_spent_label=tk.Label(continue_file_frame,text="Time Spent")
+time_spent_label.grid(row=1,column=1)
+
+for widget in continue_file_frame.winfo_children():
+    widget.grid_configure(padx=10,pady=10)
+
+# label frame for new save file
+new_save_frame=tk.LabelFrame(savefile,text="New Save File")
+new_save_frame.grid(row=1,column=0,padx=20,pady=20,sticky='nsew')
+
+new_game_button=tb.Button(new_save_frame,text="New File", bootstyle='secondary',command=lambda: mainscreen.tkraise(), cursor="hand2")
+new_game_button.grid(row=0,column=0,sticky='ew')
+
+# will update to show animals name
+animal_placeholder=tk.Label(new_save_frame,text="_____")
+animal_placeholder.grid(row=0,column=1)
+
+# will update to show total time spent since "adoption"
+time_placeholder=tk.Label(new_save_frame,text="______")
+time_placeholder.grid(row=1,column=1)
+
+for widget in new_save_frame.winfo_children():
+    widget.grid_configure(padx=10,pady=10)
+
+home_button2=tb.Button(savefile, text="Home",bootstyle = 'light',command=lambda: homescreen.tkraise(),cursor='hand2')
+home_button2.grid(row=2, column=0, sticky='sw',padx=5,pady=5)
+
+mainscreen.rowconfigure(0)  
+mainscreen.rowconfigure(1)  
+mainscreen.rowconfigure(2)
+mainscreen.columnconfigure(0)
+mainscreen.columnconfigure(1)
+mainscreen.columnconfigure(2)
+
+# label frame containing play/feed/sleep buttons
+activity_buttons_frame=tk.LabelFrame(mainscreen, text="Activity")
+activity_buttons_frame.grid(row=1,column=1,padx=20,pady=(20,10),sticky='n')
+
+# all 3 buttons need additional frame
+play_button=tb.Button(activity_buttons_frame,text="Play",bootstyle='info',cursor='hand2')
+play_button.grid(row=0,column=0,sticky='ew')
+
+feed_button=tb.Button(activity_buttons_frame,text="Feed",bootstyle='warning',cursor='hand2')
+feed_button.grid(row=0,column=1,sticky='ew')
+
+sleep_button=tb.Button(activity_buttons_frame,text="Sleep",bootstyle='primary',cursor='hand2')
+sleep_button.grid(row=0,column=2,sticky='ew')
+
+for widget in activity_buttons_frame.winfo_children():
+    widget.grid_configure(padx=5,pady=10)
+
+home_button3=tb.Button(mainscreen, text="Home",bootstyle = 'light',command=lambda: homescreen.tkraise(),cursor='hand2')
+home_button3.grid(row=2, column=0, sticky='sw',padx=20,pady=20)
 
 homescreen.tkraise()
-
-root.geometry("700x500")
-root.title("Tomogatchi")
-root.grid_columnconfigure(0, weight=1)
-root.resizable(False, False)
-root.mainloop()
-root.geometry("700x700")
 root.title("Tomogatchi")
 root.resizable(False, False)
 root.mainloop()
